dc5465d3b9726b55c3e3c9422c900987fb65a3fd
osn <cosoygnoy@gmail.com>
2021-08-27 16:38:14 +0300

    добавил кнопку смены темы

--- app/src/main/java/com/monsieur/cloy/cryptostate/MainActivity.kt
+++ app/src/main/java/com/monsieur/cloy/cryptostate/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.monsieur.cloy.cryptostate
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.ViewModelProvider
 import com.monsieur.cloy.cryptostate.databinding.ActivityMainBinding
 import com.monsieur.cloy.cryptostate.ui.assets.AddAsset
@@ -28,18 +30,48 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_fragment_menu, menu)
+        val sharedPreferences = getSharedPreferences("theme", MODE_PRIVATE)
         if (menu != null) {
+            if(sharedPreferences.getBoolean("dark", false)){
+                val item = menu.getItem(1)
+                item.icon = getDrawable(R.drawable.ic_round_light_mode_24)
+            }
             toolbarMenu = menu
         }
         return super.onCreateOptionsMenu(menu)
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun changeTheme(){
+        val sharedPreferences = getSharedPreferences("theme", MODE_PRIVATE)
+        if(sharedPreferences.getBoolean("dark", false)){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            sharedPreferences.edit().putBoolean("light", true).apply()
+            sharedPreferences.edit().putBoolean("dark", false).apply()
+        }
+        else if(sharedPreferences.getBoolean("light", false)){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            sharedPreferences.edit().putBoolean("dark", true).apply()
+            sharedPreferences.edit().putBoolean("light", false).apply()
+        }
+        else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            sharedPreferences.edit().putBoolean("light", true).apply()
+            sharedPreferences.edit().putBoolean("dark", false).apply()
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
                 backButton()
+                true
+            }
+            R.id.action_mode -> {
+                changeTheme()
                 true
             }
             R.id.action_refresh -> {
